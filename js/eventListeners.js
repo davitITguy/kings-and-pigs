80d9da256a9b7cc9a2523722b44e702786b32f93
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2022-12-23 02:46:45 +0400

    enter keydown event

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,50 +1,53 @@
-window.addEventListener('keydown', (event) => {
-  if (player.preventInput) return
-  switch (event.key) {
-    case 'w':
-      for (let i = 0; i < doors.length; i++) {
-        const door = doors[i]
+window.addEventListener('keydown', event => {
+    if (player.preventInput) return;
+    switch (event.key) {
+        case 'w':
+            for (let i = 0; i < doors.length; i++) {
+                const door = doors[i];
 
-        if (
-          player.hitbox.position.x + player.hitbox.width <=
-            door.position.x + door.width &&
-          player.hitbox.position.x >= door.position.x &&
-          player.hitbox.position.y + player.hitbox.height >= door.position.y &&
-          player.hitbox.position.y <= door.position.y + door.height
-        ) {
-          player.velocity.x = 0
-          player.velocity.y = 0
-          player.preventInput = true
-          player.switchSprite('enterDoor')
-          door.play()
-          return
-        }
-      }
-      if (player.velocity.y === 0) player.velocity.y = -25
+                if (
+                    player.hitbox.position.x + player.hitbox.width <= door.position.x + door.width &&
+                    player.hitbox.position.x >= door.position.x &&
+                    player.hitbox.position.y + player.hitbox.height >= door.position.y &&
+                    player.hitbox.position.y <= door.position.y + door.height
+                ) {
+                    player.velocity.x = 0;
+                    player.velocity.y = 0;
+                    player.preventInput = true;
+                    player.switchSprite('enterDoor');
+                    door.play();
+                    return;
+                }
+            }
+            if (player.velocity.y === 0) player.velocity.y = -25;
 
-      break
-    case 'a':
-      // move player to the left
-      keys.a.pressed = true
-      break
-    case 'd':
-      // move player to the right
-      keys.d.pressed = true
-      break
-  }
-})
+            break;
+        case 'a':
+            // move player to the left
+            keys.a.pressed = true;
+            break;
+        case 'd':
+            // move player to the right
+            keys.d.pressed = true;
+            break;
+        case 'Enter':
+            // move player to the right
+            window.location = 'https://www.spanderashvili.com';
+            break;
+    }
+});
 
-window.addEventListener('keyup', (event) => {
-  switch (event.key) {
-    case 'a':
-      // move player to the left
-      keys.a.pressed = false
+window.addEventListener('keyup', event => {
+    switch (event.key) {
+        case 'a':
+            // move player to the left
+            keys.a.pressed = false;
 
-      break
-    case 'd':
-      // move player to the right
-      keys.d.pressed = false
+            break;
+        case 'd':
+            // move player to the right
+            keys.d.pressed = false;
 
-      break
-  }
-})
+            break;
+    }
+});
